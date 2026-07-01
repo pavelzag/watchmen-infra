d2d5373fc66ac50e9178096bcadbd92d4713ecb2
Pavel Zagalsky <526113+pavelzag@users.noreply.github.com>
2026-07-01 20:59:13 +0700

    fix: generate watchmen-security-fixes.tf to remediate "Firewall Rule Open to the Internet"

---
+++ watchmen-security-fixes.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 4.0.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "watchmen-test-488807"
+}
+
+resource "google_compute_firewall" "default-allow-icmp" {
+  name    = "default-allow-icmp"
+  project = "watchmen-test-488807"
+  network = "default"
+
+  allow {
+    protocol = "icmp"
+  }
+
+  source_ranges = ["10.0.0.0/8"]
+
+  description = "Allow ICMP traffic from internal IP ranges only - restricted from 0.0.0.0/0"
+}
